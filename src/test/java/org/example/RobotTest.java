512797a714ab482f3857a02676d78aa31d989884
Shriman Vinayagamoorthy <shriman30@hotmail.com>
2023-01-16 12:14:21 -0500

    Created RobotTest.java and added dependency in Pom.xml

---
+++ src/test/java/org/example/RobotTest.java
@@ -0,0 +1,36 @@
+package org.example;
+
+class RobotTest {
+
+    @org.junit.jupiter.api.Test
+    void isInputParameterNumeric() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void initializeEnvironment() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setFacingDirection() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void moveRobotForward() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setPen() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void drawOnFloor() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void printRobotStatus() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void printFloor() {
+    }
+}
