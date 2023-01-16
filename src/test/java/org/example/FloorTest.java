596b9b4c6516b54c2f74258564deb147bc18b66d
Shriman Vinayagamoorthy <shriman30@hotmail.com>
2023-01-16 12:57:11 -0500

    Created FloorTest.java

---
+++ src/test/java/org/example/FloorTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FloorTest {
+
+    @Test
+    void drawOnFloor() {
+    }
+
+    @Test
+    void initializeFloor() {
+    }
+
+    @Test
+    void printFloor() {
+    }
+}
